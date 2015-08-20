e6bc16971c6656660d91d27d9d58bfac207747b6
zxy7107 <ivonna7107@gmail.com>
2015-08-20 17:32:14 +0800

    add content

--- HTML5-Mobile-Development-Cookbook/examples/1963_04_code/js/helper.js
+++ HTML5-Mobile-Development-Cookbook/examples/1963_04_code/js/helper.js
@@ -25,10 +25,16 @@ MBP.gestureStart = function () {
 // Hide URL Bar for iOS and Android by Scott Jehl
 // https://gist.github.com/1183357
 
+//【小云笔记】ch04r07 
 MBP.hideUrlBar = function () {
 	var win = window,
 		doc = win.document;
 
+    //检测url中是否有锚点
+    //如果有：不应该运行该代码，因为元本页面根据锚点设置的定位会被覆盖；
+    //如果没有：稍等片刻以确定页面没有滚动，这种自动滚动常常发生在页面刷新后，浏览器根据刷新前的定位自动滚动
+    //Android设备上的1像素纵向滚动
+    //iOS设备上的0像素纵向滚动来隐藏地址
 	// If there's a hash, or addEventListener is undefined, stop here
 	if( !location.hash || !win.addEventListener ){
 
@@ -58,6 +64,7 @@ MBP.hideUrlBar = function () {
 // Fast Buttons - read wiki below before using
 // https://github.com/shichuan/mobile-html5-boilerplate/wiki/JavaScript-Helper
 
+//【小云笔记】ch04r06入口函数，用途是为页面添加一个事件监听器，监测页面上发生的所有touchstart和click事件
 MBP.fastButton = function (element, handler) {
     this.element = element;
     this.handler = handler;
@@ -66,7 +73,7 @@ MBP.fastButton = function (element, handler) {
       element.addEventListener('click', this, false);
     }
 };
-
+//【小云笔记】一般情况下 大家在使用addEventListener这个方法的时候，在第二个参数上会使用一个方法，但是，还可以传入一个对象，并且对象中handleEvent为事件处理函数
 MBP.fastButton.prototype.handleEvent = function(event) {
     switch (event.type) {
         case 'touchstart': this.onTouchStart(event); break;
@@ -76,6 +83,7 @@ MBP.fastButton.prototype.handleEvent = function(event) {
     }
 };
 
+//【小云笔记】onTouchStart方法用来监听touchmove和touchend事件，StopPropagation函数用来防止事件在监听器中传递，阻止冒泡，即只在当前方法体内寻找当前事件的监听器
 MBP.fastButton.prototype.onTouchStart = function(event) {
     event.stopPropagation();
     this.element.addEventListener('touchend', this, false);
@@ -85,6 +93,7 @@ MBP.fastButton.prototype.onTouchStart = function(event) {
     this.element.style.backgroundColor = "rgba(0,0,0,.7)";
 };
 
+//【小云笔记】onTouchMove方法是用来监测浏览器上是否有拖曳事件的发生，如果用户拖曳超过10个像素，将会充值页面
 MBP.fastButton.prototype.onTouchMove = function(event) {
     if(Math.abs(event.touches[0].clientX - this.startX) > 10 || 
        Math.abs(event.touches[0].clientY - this.startY) > 10    ) {
@@ -92,6 +101,7 @@ MBP.fastButton.prototype.onTouchMove = function(event) {
     }
 };
 
+//【小云笔记】下面这段代码用来防止幽灵点击，从而可以响应真正的点击事件
 MBP.fastButton.prototype.onClick = function(event) {
     event.stopPropagation();
     this.reset();
--- HTML5-Mobile-Development-Cookbook/notes.md
+++ HTML5-Mobile-Development-Cookbook/notes.md
@@ -6,7 +6,7 @@
 
 
 <a name="chapter1">第1章 HTML5与移动网站
-
+---
 
 
 
@@ -15,7 +15,7 @@
 
 
 <a name="chapter2">第2章 移动端的配置和优化
-
+---
 
 
 
@@ -26,20 +26,30 @@
 
 
 <a name="chapter3">第3章 移动设备的交互方式
+---
 
 
 
-<a name="chapter4">第4章 构建快速响应式移动互联网站点
-> <a name="tips49">4.9 加速按钮反馈 p96
-
+<a name="chapter4">第4章 构建快速响应式移动互联网站点>
+---
+<a name="tips409">4.9 加速按钮反馈 p96
 [ch04r06.html](examples/1963_04_code/ch04r06.html)<br>
 [helper.js](examples/1963_04_code/js/helper.js)<br>
-Google博客关于加速按钮反馈的理念和背景：[http://code.google.com/mobile/articles/fast_buttons.html](http://code.google.com/mobile/articles/fast_buttons.html)
+~~Google博客关于加速按钮反馈的理念和背景：[http://code.google.com/mobile/articles/fast_buttons.html](http://code.google.com/mobile/articles/fast_buttons.html)~~<br>
+[https://developers.google.com/web/updates/2013/12/300ms-tap-delay-gone-away](https://developers.google.com/web/updates/2013/12/300ms-tap-delay-gone-away)<br>
+[http://x1024.github.io/jquery-fastbutton/](http://x1024.github.io/jquery-fastbutton/)
+[石川blog:http://www.blog.highub.com/mobile-2/a-fix-for-iphone-viewport-scale-bug/](http://www.blog.highub.com/mobile-2/a-fix-for-iphone-viewport-scale-bug/)
+[demo:http://www.lab.highub.com/m/scalefix.html](http://www.lab.highub.com/m/scalefix.html)
+***
+<a name="tips409">4.10 隐藏浏览器的地址栏 p99
+[ch04r07.html](examples/1963_04_code/ch04r07.html)<br>
+[helper.js](examples/1963_04_code/js/helper.js)<br>
+~~&lt;script src="script//ajax.googleapis.com/ajax/libs/jquery/1.5.1/jquery.js&gt;&lt;/script&gt;~~
 
 
 
 <a name="chapter5">第5章 移动设备访问
-
+---
 
 
 
@@ -48,8 +58,9 @@ Google博客关于加速按钮反馈的理念和背景：[http://code.google.com
 
 
 <a name="chapter6">第6章 移动富媒体
-
+---
 <a name="chapter7">第7章 移动设备调试
+---
 - 7.1 简介
 - 7.2 使用Opera Dragonfly远程调试
 - 7.3 使用weinre远程调试
@@ -63,9 +74,10 @@ Google博客关于加速按钮反馈的理念和背景：[http://code.google.com
 - 7.6 设置移动Safari调试器
 
 <a name="chapter8">第8章 服务器端性能调优
-
+---
 
 <a name="chapter9">第9章 移动性能测试
-
+---
 
 <a name="chapter10">第10章 拥抱移动互联网特性
+---
